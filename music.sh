5e0a15d072f2545b7f45cdcdf8978036bfbf6970
CDWEN0526 <58065939+CDWEN0526@users.noreply.github.com>
2020-01-02 17:14:13 +0800

    Add files via upload

--- music.sh
+++ music.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`
 while true;do 
 echo -e "\n==树莓派自动采集音乐程序=="
-echo -e "  1、切换歌曲\n  2、删除歌曲\n  3、调整音量\n  4、播放音乐\n  5、采集音乐\n  6、退出系统\n"
+echo -e "  1、切换歌曲\n  2、删除歌曲\n  3、调整音量\n  4、播放音乐\n  5、采集音乐\n  6、停止播放\n  7、退出系统\n"
 read -p "请输入您的操作：" number
 echo -e  "\n=======开始执行操作=======\n"
 case $number in 
@@ -39,7 +39,12 @@ sudo ./kill_mp3.sh
 sleep 3
 ;;
 "6")
-echo "退出操作..."
+sudo ./kill_all_mp3.sh
+echo "关闭播放"
+exit 0
+;;
+"7")
+echo "退出后台运行..."
 exit 0
 ;;
 *)
